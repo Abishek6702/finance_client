048f69c08d56c5559441b5e4bb1b8338e74e1ab0
dharshan-m <dharshanmohan2704@gmail.com.com>
2026-02-11 15:05:09 +0530

    developed login button in landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const Landing = () => {
+const Landing = () => { 
+  const navigate = useNavigate()
+  const handleLogin=()=>{
+    navigate('/login')
+  }
   return (
-    <div>
-     Landing 
+    <div className='flex justify-center items-center w-full h-screen '  >
+     <button className='boder border-[#d9d9d9] bg-[#0b56a4] text-white text-xl rounded-xl p-3 px-6 cursor-pointer' onClick={handleLogin}>Login</button>
     </div>
   )
 }
