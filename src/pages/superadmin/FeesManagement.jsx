bb5fa144abce9c808829db6c740e7da80981841e
Abishek6702 <abishekkrishnat@gmail.com>
2026-02-28 10:47:21 +0530

    ak 28th nodata and empty1

--- src/pages/superadmin/FeesManagement.jsx
+++ src/pages/superadmin/FeesManagement.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Underdevelopment from '../../components/general/Underdevelopement'
 
 const FeesManagement = () => {
   return (
-    <div>
-      
-    </div>
+    <div className="flex items-center justify-center h-[calc(100vh-120px)]">
+    <Underdevelopment head={"Fees Management"} />
+  </div>
   )
 }
 
--- src/pages/superadmin/SuperDashboard.jsx
+++ src/pages/superadmin/SuperDashboard.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Underdevelopment from "../../components/general/Underdevelopement";
 
 const SuperDashboard = () => {
   return (
-    <div>
-      
+    <div className="flex items-center justify-center h-[calc(100vh-120px)]">
+      <Underdevelopment head={"Admin Dashboard"} />
     </div>
-  )
-}
+  );
+};
 
-export default SuperDashboard
+export default SuperDashboard;
