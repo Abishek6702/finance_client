9aabaeb8e19bda6a4d74ed55ee747c4f62178d44
Abishek6702 <abishekkrishnat@gmail.com>
2026-04-11 10:29:04 +0530

    admin&superadmin dashboard integration over

--- src/pages/superadmin/SuperDashboard.jsx
+++ src/pages/superadmin/SuperDashboard.jsx
@@ -1,12 +1,31 @@
+
 import React from "react";
-import Underdevelopment from "../../components/general/Underdevelopement";
+
+import CardComponent from "../../components/dashboard/CardComponent.jsx";
+import DepartmentPaid from "../../components/dashboard/DepartmentPaid.jsx";
+import DepartmentCount from "../../components/dashboard/DepartmentCount.jsx";
+import Timingpaid from "../../components/dashboard/Timingpaid.jsx";
 
 const SuperDashboard = () => {
   return (
-    <div className="flex items-center justify-center h-[calc(100vh-120px)]">
-      <Underdevelopment head={"Admin Dashboard"} />
-      Welcoe to Super Admin Dashboard
-    </div>
+    <>
+      <div className="flex flex-col  items-center h-[calc(100vh-120px)] space-y-4">
+        <div className="flex justify-center gap-4 w-full h-auto">
+          <div className="card-container border border-gray-200  w-[60%] bg-[#fafbfc] rounded-xl">
+            <CardComponent />
+          </div>
+          <div className="card-container border border-gray-200  w-[40%] bg-[#fafbfc] rounded-xl">
+            <DepartmentCount />
+          </div>
+        </div>
+        <div className="departmnt-paid w-full border border-gray-200 rounded-xl h-auto">
+          <DepartmentPaid />
+        </div>
+        {/* <div className="departmnt-paid w-full border border-gray-200 rounded-xl h-auto">
+          <Timingpaid />
+        </div> */}
+      </div>
+    </>
   );
 };
 
